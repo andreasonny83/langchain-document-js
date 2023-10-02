ceeca6878eb152c0b006e172e86e3517fff5ecf6
andreasonny83 <andreasonny83@gmail.com>
2023-10-02 11:02:07 +0100

    Add Prettier

--- index.js
+++ index.js
@@ -8,31 +8,38 @@ import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 const WIKI_PAGE = '2023_Hawaii_wildfires';
 
-try {
-  // Retrieve the document from Wikipedia
-  const loader = new CheerioWebBaseLoader(`https://en.wikipedia.org/wiki/${WIKI_PAGE}`);
-  const data = await loader.load();
+(async () => {
+  try {
+    // Retrieve the document from Wikipedia
+    const loader = new CheerioWebBaseLoader(`https://en.wikipedia.org/wiki/${WIKI_PAGE}`);
+    const data = await loader.load();
 
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    chunkOverlap: 20,
-  });
-  const splitDocs = await textSplitter.splitDocuments(data);
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 20,
+    });
+    const splitDocs = await textSplitter.splitDocuments(data);
 
-  // Then use the TensorFlow Embedding to store these chunks in the datastore
-  const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, new TensorFlowEmbeddings());
-  const retriever = vectorStore.asRetriever();
+    // Then use the TensorFlow Embedding to store these chunks in the datastore
+    const vectorStore = await MemoryVectorStore.fromDocuments(
+      splitDocs,
+      new TensorFlowEmbeddings(),
+    );
+    const retriever = vectorStore.asRetriever();
 
-  const ollama = new Ollama({
-    baseUrl: 'http://localhost:11434',
-    model: 'llama2',
-  });
+    const ollama = new Ollama({
+      baseUrl: 'http://localhost:11434',
+      model: 'llama2',
+    });
 
-  const chain = RetrievalQAChain.fromLLM(ollama, retriever);
+    const chain = RetrievalQAChain.fromLLM(ollama, retriever);
 
-  const result = await chain.call({ query: "When was Hawaii's request for a major disaster declaration approved?" });
+    const result = await chain.call({
+      query: "When was Hawaii's request for a major disaster declaration approved?",
+    });
 
-  console.log(result.text);
-} catch (e) {
-  console.error(e.message);
-}
+    console.log(result.text);
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
